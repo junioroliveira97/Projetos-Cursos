5ca118103321dd762a06a841572c198a2b6e3a22
Seu nome aqui <juniorjr97@outlook.com>
2022-03-03 11:20:32 -0300

    Lista de cursos e atividades

---
+++ Fundamentos de Javascript/funcoes.js
@@ -0,0 +1,26 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// declara a função 
+                        //2 -> valorde soma()
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+// imprimeTexto("outro texto");
+
+// três formas de escrever funções
+
+function soma(){
+    // outros codigos 
+    // vários consoles.log()
+    return 2 + 2;
+}
+
+// console.log(soma())
